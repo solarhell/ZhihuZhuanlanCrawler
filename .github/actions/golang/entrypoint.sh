d9df3177c47454e5fa19e12bf5e641483cbc9724
solarhell <songjiaxin2008@gmail.com>
2019-03-19 14:47:00 +0800

    test GitHub Actions

--- .github/actions/golang/entrypoint.sh
+++ .github/actions/golang/entrypoint.sh
@@ -8,6 +8,13 @@ export GO111MODULE=on
 go mod tidy
 go mod verify
 
+if [[ "$1" == "lint" ]]; then
+    echo "#######################"
+    echo "# Running GolangCI-Lint"
+    golangci-lint --version
+    golangci-lint run --enable-all --disable gochecknoglobals --disable gochecknoinits
+fi
+
 if [[ "$1" == "test" ]]; then
     echo "#######################"
     echo "# Running Test"
