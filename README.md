b62fd0eeb69ad91f6fe9856ffca6b1bfe907cc10
songjiaxin <songjiaxin@gmail.com>
2020-08-07 10:09:24 +0800

    修复一些测试和结构体

--- README.md
+++ README.md
@@ -3,53 +3,10 @@
 
 ![screenshot1.png](misc/screenshot1.png)
 
-## usage
+## 测试爬虫功能
 
-```go
-package main
-
-import (
-	"fmt"
-	"log"
-	"os"
-	Zhihu "github.com/solarhell/ZhihuZhuanlanCrawler"
-)
-
-func main() {
-	const columnName = "OTalk"
-
-	pinnedArticlePidAndAuthor, err := Zhihu.GetPinnedArticlePidAndAuthor(columnName)
-	if err != nil {
-		log.Println(err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("%+v\n", *pinnedArticlePidAndAuthor)
-
-	pinnedArticle, err := Zhihu.GetSingleArticle(pinnedArticlePidAndAuthor.ID)
-	if err != nil {
-		log.Println(err)
-		os.Exit(1)
-	}
-
-	fmt.Printf("%+v\n", *pinnedArticle)
-
-	pids, err := Zhihu.GetArticlesListPids(columnName)
-	if err != nil {
-		log.Println(err)
-		os.Exit(1)
-	}
-
-	for _, pid := range pids {
-		if pid == pinnedArticle.ID {
-			continue
-		}
-		article, err := Zhihu.GetSingleArticle(pid)
-		if err != nil {
-			log.Println(err)
-			os.Exit(1)
-		}
-		fmt.Printf("%+v\n", *article)
-	}
-}
+```shell script
+go test -run TestCrawl -v
 ```
+
+具体用法可以参考 `crawler_test.go`。
--- crawler_test.go
+++ crawler_test.go
@@ -1,6 +1,8 @@
 package ZhihuZhuanlanCrawler
 
-import "testing"
+import (
+	"testing"
+)
 
 const columnName = "OTalk" // https://zhuanlan.zhihu.com/Otalk
 const pid = 60968502       // https://zhuanlan.zhihu.com/p/60968502
@@ -20,7 +22,7 @@ func TestClient_GetArticlesListPids(t *testing.T) {
 		t.Error(err.Error())
 		return
 	}
-	t.Logf("%+v\n", pids)
+	t.Logf("%v\n", pids)
 }
 
 func TestClient_GetSingleArticle(t *testing.T) {
@@ -30,4 +32,44 @@ func TestClient_GetSingleArticle(t *testing.T) {
 		return
 	}
 	t.Logf("%+v\n", *article)
+}
+
+func TestCrawl(t *testing.T) {
+	const columnName = "OTalk"
+
+	pinnedArticlePidAndAuthor, err := GetPinnedArticlePidAndAuthor(columnName)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	t.Logf("%+v\n", *pinnedArticlePidAndAuthor)
+
+	pinnedArticle, err := GetSingleArticle(pinnedArticlePidAndAuthor.ID)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	t.Logf("%+v\n", *pinnedArticle)
+
+	pids, err := GetArticlesListPids(columnName)
+	if err != nil {
+		t.Error(err)
+		return
+	}
+
+	t.Logf("pids: %v\n", pids)
+
+	for _, pid := range pids {
+		if pid == pinnedArticle.ID {
+			continue
+		}
+		article, err := GetSingleArticle(pid)
+		if err != nil {
+			t.Error(err)
+			return
+		}
+		t.Logf("%+v\n", *article)
+	}
 }
